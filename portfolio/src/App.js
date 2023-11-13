1085a61556748a7666362e194e1e1853405d084c
dizas9 <sazid.ice07@gmail.com>
2023-11-13 18:02:02 +0600

    Create Project With create-react-app

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <p className="demo">Hello</p>
+    <section className="sec">Sction</section>
+    </>
+  )
+}
+
+export default App;
